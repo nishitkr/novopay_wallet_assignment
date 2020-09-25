047844b22a42197f93d2a9953d81f8d838d454e6
I331559 <nishit.kumar@sap.com>
2020-09-25 21:07:31 +0530

    First commit

---
+++ src/main/java/com/nishit/novopay/NovopayWalletApplication.java
@@ -0,0 +1,13 @@
+package com.nishit.novopay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovopayWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovopayWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nishit/novopay/NovopayWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nishit.novopay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovopayWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
